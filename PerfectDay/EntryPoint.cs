a21ccc58ea784af05aff62ca6c577a439677b06e
Smoke <smokey.edgy@gmail.com>
2018-03-28 01:27:08 -0400

    Zombies now walk like zombies and stumble towards their target

--- PerfectDay/EntryPoint.cs
+++ PerfectDay/EntryPoint.cs
@@ -27,6 +27,18 @@ namespace PerfectDay
                 GameFiber.StartNew(EntryPoint.SpawnZombie);
         }
 
+        [Rage.Attributes.ConsoleCommand(Description = "Drops health", Name = "DropHealth")]
+        public static void Command_DropHealth()
+        {
+            Ped ped = GetClosestLivingHumanNear(Game.LocalPlayer.Character);
+
+            //ApplyPedDamagePack(GetPlayerPed(-1), "Fall", 100, 100);
+            Rage.Native.NativeFunction.Natives.APPLY_PED_DAMAGE_PACK(ped, "Fall", 100, 100);
+            AnimationSet animationSet = new AnimationSet("move_m@drunk@verydrunk");
+
+            ped.MovementAnimationSet = animationSet;            
+        }
+
         public static Ped GetClosestLivingHumanNear(Ped ped)
         {
             Ped[] nearbyPeds = ped.GetNearbyPeds(16);
@@ -41,11 +53,20 @@ namespace PerfectDay
                 
         public static void SpawnZombie()
         {
-
+                        
             Vector3 spawnPosition = World.GetNextPositionOnStreet(Game.LocalPlayer.Character.GetOffsetPositionFront(100f));
             Model zombieModel = new Model("u_m_y_zombie_01");
-
-            Ped zombie = new Ped(zombieModel, spawnPosition, 0);
+            
+            Ped zombie = GetClosestLivingHumanNear(Game.LocalPlayer.Character);
+            Rage.Native.NativeFunction.Natives.APPLY_PED_DAMAGE_PACK(zombie, "Fall", 100, 100);
+            AnimationSet animationSet = new AnimationSet("move_m@drunk@verydrunk");
+            animationSet.LoadAndWait();
+            zombie.MovementAnimationSet = animationSet;
+            
+            if (zombie == null)
+            {
+                return;
+            }
 
             zombie.RelationshipGroup = ZombiesGroup;
             zombie.StaysInGroups = false;
@@ -68,7 +89,7 @@ namespace PerfectDay
                         {
                             Ped closestHuman = GetClosestLivingHumanNear(zombie);
                             if (closestHuman != null)
-                                zombie.Tasks.FightAgainst(closestHuman).WaitForCompletion();
+                                zombie.Tasks.FollowToOffsetFromEntity(closestHuman, Vector3.Zero).WaitForCompletion();
                             else
                                 zombie.Tasks.Wander();
                         }                        
