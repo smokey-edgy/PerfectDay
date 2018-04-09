99306c47233305332b7f2874a67c1ba2caf32bda
Smoke <smokey.edgy@gmail.com>
2018-04-08 21:26:17 -0400

    Created mayhem scenario with traffic density increase and some cleanup

--- PerfectDay/EntryPoint.cs
+++ PerfectDay/EntryPoint.cs
@@ -29,220 +29,50 @@ namespace PerfectDay
 
             GameFiber.WaitUntil(IsGameReady);
             //GameFiber.StartNew(SpawnZombie);
-            GameFiber.StartNew(CreateTrafficJams3);
-            GameFiber.StartNew(() => {
-                foreach (Vehicle vehicle in World.GetAllVehicles())
+            GameFiber.StartNew(IncreaseTraffic);
+            //GameFiber.StartNew(CreateMayhemAndPanic);
+        }
+
+        [Rage.Attributes.ConsoleCommand(Description = "Create mayhem and panic", Name = "Mayhem")]
+        public static void CreateMayhemAndPanic()
+        {
+            while (true)
+            {
+                Entity[] nearbyCars = World.GetEntities(Game.LocalPlayer.Character.GetOffsetPositionFront(200.0f), 150.0f, GetEntitiesFlags.ConsiderCars);
+
+                foreach (Vehicle vehicle in nearbyCars)
                 {
                     if (vehicle && vehicle.IsValid())
                     {
                         Ped ped = vehicle.Driver;
                         if (ped && ped.IsValid() && !ped.IsLocalPlayer)
                         {
-                            vehicle.EngineHealth = 0.0f;
+                            ped.BlockPermanentEvents = true;
                             ped.Tasks.Clear();
-                            Rage.Native.NativeFunction.Natives.SET_DRIVE_TASK_DRIVING_STYLE(ped, 1074528293);
-                            Rage.Native.NativeFunction.Natives.ADD_SHOCKING_EVENT_AT_POSITION<uint>(116, ped.Position, 0);
+                            TaskSequence taskSequence = new TaskSequence(ped);
+                            taskSequence.Tasks.LeaveVehicle(vehicle, LeaveVehicleFlags.LeaveDoorOpen);
+                            taskSequence.Tasks.ReactAndFlee(Game.LocalPlayer.Character);
+                            taskSequence.Execute();
+                            Rage.Native.NativeFunction.Natives.ADD_SHOCKING_EVENT_FOR_ENTITY<uint>(86, vehicle, 0);
                         }
                     }
                 }
-
-                GameFiber.Sleep(10000);
-
-            });
+                GameFiber.Sleep(5000);
+            }
         }
 
-        [Rage.Attributes.ConsoleCommand(Description = "Start a traffic jam", Name = "TrafficJam")]
-        public static void CreateTrafficJamsCommand()
-        {
-            GameFiber.StartNew(CreateTrafficJams3);
-        }
-
-        public static void CreateTrafficJams3()
-        {
-            //Game.LocalPlayer.Character.Tasks.Clear();
-           
-            //Rage.Native.NativeFunction.Natives.ADD_SHOCKING_EVENT_AT_POSITION<uint>(86, Game.LocalPlayer.Character.Position, 0);
-
+        public static void IncreaseTraffic()
+        {           
             while (true)
             {
                 Rage.Native.NativeFunction.Natives.SET_RANDOM_VEHICLE_DENSITY_MULTIPLIER_THIS_FRAME(100.0f);
                 Rage.Native.NativeFunction.Natives.SET_VEHICLE_DENSITY_MULTIPLIER_THIS_FRAME(100.0f);
-                //Rage.Native.NativeFunction.Natives._SET_SOME_VEHICLE_DENSITY_MULTIPLIER_THIS_FRAME(2);
-                //Rage.Native.NativeFunction.Natives.SET_PARKED_VEHICLE_DENSITY_MULTIPLIER_THIS_FRAME(2);
                 Rage.Native.NativeFunction.Natives.SET_FAR_DRAW_VEHICLES(1);
                 Rage.Native.NativeFunction.Natives.POPULATE_NOW();
-                //foreach (Vehicle vehicle in World.GetAllVehicles())
-                //{
-                //    vehicle.EngineHealth = 0.0f;
-                //}
-               
                 GameFiber.Yield();
             }
-
         }
-
-        public static void CreateTrafficJams2()
-        {
-            Game.Console.Print("****** About to create Traffic jams");
-
-            int numberOfVehiclesToSpawn = 10;
-            
-            while (numberOfVehiclesToSpawn > 0)
-            {
-                Vector3 somewhereAroundThePlayer = Game.LocalPlayer.Character.Position.Around(MathHelper.GetRandomSingle(10f, 20f));
-                float z;
-                if (Rage.Native.NativeFunction.Natives.GET_GROUND_Z_FOR_3D_COORD<bool>(somewhereAroundThePlayer.X, somewhereAroundThePlayer.Y, somewhereAroundThePlayer.Z, out z, false))
-                {
-                    somewhereAroundThePlayer.Z = z;
-                }
-                bool pointIsOnRoad = Rage.Native.NativeFunction.Natives.IS_POINT_ON_ROAD<bool>(somewhereAroundThePlayer.X, somewhereAroundThePlayer.Y, somewhereAroundThePlayer.Z);
-
-                if (!pointIsOnRoad)
-                    continue;
-
-                Entity[] entitiesInPossibleSpawnLocation = World.GetEntities(somewhereAroundThePlayer, 10, GetEntitiesFlags.ConsiderAllObjects);
-                if (entitiesInPossibleSpawnLocation.Length > 0)
-                    continue;
-
-                Vehicle vehicle = new Vehicle((Model model) => { return model.IsCar && !model.IsBigVehicle; }, somewhereAroundThePlayer);
-                Ped ped = vehicle.CreateRandomDriver();
-                vehicle.IsEngineOn = true;
-                ped.Tasks.CruiseWithVehicle(20.0f);
-                numberOfVehiclesToSpawn--;                
-            }
-        }
-
-        public static void CreateTrafficJams()
-        {
-            Game.Console.Print("****** About to create Traffic jams");            
-
-            int numberOfVehiclesToSpawn = 100;
-
-            Vehicle[] nearbyVehicles = Game.LocalPlayer.Character.GetNearbyVehicles(1);
-            if (nearbyVehicles.Length == 0)
-                return;
-
-            Vehicle nearbyVehicle = nearbyVehicles[0];
-
-            while (numberOfVehiclesToSpawn > 0)
-            {
-                if (nearbyVehicle.IsValid())
-                {
-                    Vector3 possibleSpawnLocation = nearbyVehicle.GetOffsetPositionFront(3);
-                    Entity[] entitiesInPossibleSpawnLocation = World.GetEntities(possibleSpawnLocation, 2, GetEntitiesFlags.ConsiderAllObjects);
-
-                    if (entitiesInPossibleSpawnLocation.Length > 0)
-                    {
-                        possibleSpawnLocation = nearbyVehicle.GetOffsetPositionRight(3);
-                        entitiesInPossibleSpawnLocation = World.GetEntities(possibleSpawnLocation, 2, GetEntitiesFlags.ConsiderAllObjects);
-                    }
-
-                    bool pointIsOnRoad = Rage.Native.NativeFunction.Natives.IS_POINT_ON_ROAD<bool>(possibleSpawnLocation.X, possibleSpawnLocation.Y, possibleSpawnLocation.Z);
-                    if (entitiesInPossibleSpawnLocation.Length == 0 && pointIsOnRoad)
-                    {
-                        Vehicle vehicle = new Vehicle((Model model) => { return model.IsCar && !model.IsBigVehicle; }, possibleSpawnLocation);
-                        Ped ped = vehicle.CreateRandomDriver();
-
-                        if (nearbyVehicle.IsValid())
-                        {
-                            vehicle.Heading = nearbyVehicle.Heading;
-                            vehicle.IsEngineOn = true;
-                            ped.Tasks.CruiseWithVehicle(nearbyVehicle.Speed);
-                            numberOfVehiclesToSpawn--;
-                            nearbyVehicle = vehicle;
-                        }
-                        else
-                        {
-                            //vehicle.Delete();
-                            ped.Delete();
-                        }
-                    }
-                    else
-                    {
-                        nearbyVehicles = Game.LocalPlayer.Character.GetNearbyVehicles(1);
-                        if (nearbyVehicles.Length == 0)
-                            return;
-
-                        nearbyVehicle = nearbyVehicles[0];
-                    }
-                }
-                GameFiber.Yield();
-            }
-          
-                
-            }
-
-
-            //Vector3 playerPos = Game.LocalPlayer.Character.GetOffsetPositionFront(0);
-            
-            //int numberOfVehiclesToSpawn = 20;
-
-            //while (numberOfVehiclesToSpawn > 0)
-            //{
-            //    Vehicle[] nearestVehicles = Game.LocalPlayer.Character.GetNearbyVehicles(10);
-            //    for (int i = 0; i < nearestVehicles.Length; i++)
-            //    {
-            //        Vehicle nearestVehicle = nearestVehicles[i];
-            //        for (int j = 0; j < 20; j++)
-            //        {
-            //            if (nearestVehicle && !nearestVehicle.IsValid())
-            //                break;
-            //            Vector3 nextPositionOnStreetNotOccupied = World.GetNextPositionOnStreet(nearestVehicle.GetOffsetPositionFront(20.0f * j));// NextPositionOnStreet(nearestVehicle.GetOffsetPositionFront(20.0f));
-            //            Entity[] entitiesNearPosition = World.GetEntities(nextPositionOnStreetNotOccupied, 5.0f, GetEntitiesFlags.ConsiderAllObjects);
-            //            if (entitiesNearPosition.Length > 0)
-            //                continue;
-
-            //            Vehicle vehicle = new Vehicle((Model model) => { return model.IsCar; }, nextPositionOnStreetNotOccupied);
-            //            vehicle.Heading = nearestVehicle.Heading;
-            //            vehicle.IsEngineOn = true;
-            //            Ped ped = vehicle.CreateRandomDriver();
-            //            ped.Tasks.CruiseWithVehicle(nearestVehicle.Speed);
-            //            numberOfVehiclesToSpawn--;
-            //            if (numberOfVehiclesToSpawn == 0)
-            //                break;
-            //            nearestVehicle = vehicle;
-            //            GameFiber.Sleep(500);
-            //        }
-            //    }
-            //}
-            //Vector3 lastVehiclePosition = World.GetNextPositionOnStreet(Game.LocalPlayer.Character.GetOffsetPositionFront(10.0f));
-            //for (int i = 0; i<2; i++)
-            //{                                
-            //    Vehicle vehicle = new Vehicle((Model model) => { return model.IsCar; }, lastVehiclePosition);
-            //    vehicle.IsEngineOn = true;
-            //    vehicle.Velocity = vehicle.ForwardVector * 2;               
-            //    Ped ped = vehicle.CreateRandomDriver();
-            //    ped.Tasks.CruiseWithVehicle(40);
-
-            //    Vehicle vehicle2 = new Vehicle((Model model) => { return model.IsCar; }, vehicle.GetOffsetPositionRight(5.0f));
-            //    vehicle2.IsEngineOn = true;
-            //    vehicle2.Velocity = vehicle2.ForwardVector * 2;
-            //    Ped ped2 = vehicle2.CreateRandomDriver();
-            //    ped2.Tasks.CruiseWithVehicle(40);
-
-            //    lastVehiclePosition = vehicle.GetOffsetPositionFront(5.0f);
-            //}
-
-
-            //while (true)
-            //{
-            //    Rage.Native.NativeFunction.Natives.SET_VEHICLE_DENSITY_MULTIPLIER_THIS_FRAME(1.3f);
-            //    Rage.Native.NativeFunction.Natives.SET_RANDOM_VEHICLE_DENSITY_MULTIPLIER_THIS_FRAME(1.3f);
-            //    Rage.Native.NativeFunction.Natives.SET_PARKED_VEHICLE_DENSITY_MULTIPLIER_THIS_FRAME(1.3f);
-            //    //World.AddSpeedZone(Game.LocalPlayer.Character.GetOffsetPositionFront(1000.0f), 100.0f, 0.0f);
-            //    Entity[] entities = World.GetEntities(Game.LocalPlayer.Character.Position, 500.0f, GetEntitiesFlags.ConsiderCars);
-            //    for (int i = 0; i < entities.Length; i++)
-            //    {
-            //        Vehicle car = (Vehicle)entities[i];
-
-            //        if (car.Driver && !car.Driver.Equals(Game.LocalPlayer.Character))
-            //        {
-            //            car.EngineHealth = 0.0f;                                                
-            //        }
-
-            //    }
-            //    GameFiber.Sleep(1000);        
-
+        
         [Rage.Attributes.ConsoleCommand(Description = "Spawns a zombie", Name = "SpawnZombie")]
         public static void Command_SpawnZombie(int howMany)
         {
