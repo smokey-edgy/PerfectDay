baadcf00ae587da13463e0d127ac1ba971f6291e
Smoke <smokey.edgy@gmail.com>
2018-11-26 23:45:32 -0500

    Added a naive closest target to pursue algorithm

--- PerfectDay/Zombie.cs
+++ PerfectDay/Zombie.cs
@@ -19,6 +19,7 @@ namespace PerfectDay
     class Zombie
     {
         private Ped Ped;
+        private Ped Target;
         private ZombieState State;
 
         private static AnimationSet ZombieWalk = new AnimationSet("move_m@drunk@verydrunk");
@@ -38,6 +39,10 @@ namespace PerfectDay
             Ped.BlockPermanentEvents = true;
             Ped.IsCollisionEnabled = true;
 
+            ZombieWalk.LoadAndWait();
+            Ped.MovementAnimationSet = ZombieWalk;
+
+            Ped.Tasks.Clear();
             Ped.Tasks.StandStill(-1);
             State = ZombieState.StandingStill;
 
@@ -57,9 +62,10 @@ namespace PerfectDay
                             Wander();
                             break;
                         case ZombieState.Wandering:
-                            LookForTargets();
+                            LookForClosestTarget();
                             break;
                         case ZombieState.Pursuing:
+                            LookForClosestTarget();
                             StartAttackingWhenCloseToTarget();
                             break;
                         case ZombieState.Attacking:
@@ -73,29 +79,41 @@ namespace PerfectDay
         private void Wander()
         {
             GameFiber.Sleep(5000);
-            ZombieWalk.LoadAndWait();
-            Ped.MovementAnimationSet = ZombieWalk;
-            Ped.Tasks.Wander();       
+            Ped.Tasks.Clear();
+            Ped.Tasks.Wander();
+            State = ZombieState.Wandering;       
         }
 
-        private void LookForTargets()
+        private void LookForClosestTarget()
         {
+            Ped[] nearbyPeds = Ped.GetNearbyPeds(1);
+            foreach (Ped nearbyPed in nearbyPeds)
+            {
+                if(nearbyPed.RelationshipGroup != ZombiesGroup)
+                {
+                    Ped.MovementAnimationSet = ZombieWalk;
+                    Ped.Tasks.Clear();
+                    Ped.Tasks.FollowToOffsetFromEntity(nearbyPed, Vector3.Zero);
+                    Target = nearbyPed;
+                    State = ZombieState.Pursuing;                    
+                }
+            }
         }
 
         private void StartAttackingWhenCloseToTarget()
         {
-
+            if (Ped.DistanceTo(Target) <= 1)
+            {
+                State = ZombieState.Attacking;
+            }       
         }
 
         private void ZombifyTarget()
         {
-
+            new Zombie(Target).Zombify();
+            State = ZombieState.Wandering;
         }
-        private void Die()
-        {
-
-        }
-
+        
         private bool NotDead()
         {
             return ZombieState.Dead != State;
